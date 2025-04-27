cb67aab44a9a9d5f535e1bc18b1872f4ca8840b9
sytheg0d <fanaticsythe@gmail.com>
2025-04-27 05:25:49 +0200

    AdminPage için token

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../App.css';
 
